3cb7ce7f34a935cc907d22c2f37f1ebb1accf213
Francisco Hernandez <frankph0198@gmail.com>
2020-09-16 13:17:09 -0500

    BubbleSort

---
+++ sort/BubbleSort.java
@@ -0,0 +1,14 @@
+public class BubbleSort{
+    
+    public void sort(int[] array){
+        for(int i = 0; i<array.length-1; i++){
+            for(int j = 0; j<array.length-1-i; j++){
+                if(array[j] > array[j+1]){
+                    int aux = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = aux;
+                }
+            }
+        }
+    }
+}
--- sort/TestSorting.java
+++ sort/TestSorting.java
@@ -1,12 +1,19 @@
 public class TestSorting{
     public static void main(String[] args) {
         MergeSort mergeSort = new MergeSort();
-
         int[] array = {8,6,4,3,1,7,2,5};
-
         mergeSort.sort(array, 0, array.length-1);
+        print(array);
 
+        int[] array2 = {8,6,4,3,1,7,2,5};
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.sort(array2);
+        print(array2);
+    }
+
+    public static void print(int[] array){
         for(int i : array)
-            System.out.print(i + ", ");
+            System.out.print(i + " ");
+        System.out.println();
     }
 }
